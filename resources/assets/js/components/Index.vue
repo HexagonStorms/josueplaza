a7a9886b5459b7db54355a596770c441cab16b0b
Josue Plaza <josue@parlevelsystems.com>
2017-11-04 19:22:48 -0500

    add laravel back with frontend scaffold of Webpack, SASS, and Vue

---
+++ resources/assets/js/components/Index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Index Component</div>
+
+                    <div class="panel-body">
+                        I'm the component!
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Index mounted.')
+        }
+    }
+</script>
